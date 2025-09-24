12f9f63ff812264ae976bde13c9b88ab7b16ec98
Ender Karan <ender.karan14@gmail.com>
2025-09-24 09:47:42 +0300

    Fix model path and track GLB with LFS

--- interface-foundry/src/components/Ayakkabi.jsx
+++ interface-foundry/src/components/Ayakkabi.jsx
@@ -9,9 +9,10 @@ Title: CONVERSE SHOE
 
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
+import modelUrl from './ayakkabi.glb' 
 
 export function Ayakkabi({ customColors, ...props }) {
-  const { nodes, materials } = useGLTF('../ayakkabi.glb')
+  const { nodes, materials } = useGLTF({modelUrl})
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={0.268}>
@@ -30,4 +31,4 @@ export function Ayakkabi({ customColors, ...props }) {
   )
 }
 
-useGLTF.preload('../ayakkabi.glb')
+useGLTF.preload({modelUrl})
